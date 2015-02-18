c51712a271d76ee65ccb4eb5575fd236be77d640
Hanna <hanna.meyer1@gmail.com>
2015-02-18 11:03:16 +0100

    initial commit

--- R/validate.R
+++ R/validate.R
@@ -1,10 +1,21 @@
+#' Validation of model results
+#' @param obs A RasterLayer of observed rainfall areas or rainfall rates
+#' @param pred A RasterLayer of predicted rainfall areas or rainfall rates
+#' @param type A character string indicating the type of the model output.
+#' Either "classification" or "regressione"
+#' @return A data.frame containing the validation statistics
+#' @description The calculations base on the regressionStats or 
+#' classificationStats of the Rsenal package
+#' @author Hanna Meyer
+#' @seealso \code{\link{classificationStats}}, \code{\link{regressionStats}}
+
 validate <- function (obs, pred, type="regression",...){
   require(Rsenal)
   if (type=="regression"){
-    regressionStats(values(pred), values(obs),adj.rsq=FALSE)
+    stats <- regressionStats(values(pred), values(obs),adj.rsq=FALSE)
   }
   if (type=="classification"){
-    classificationStats(values(pred), values(obs))
+    stats <- classificationStats(values(pred), values(obs))
   }
-  
+  return(stats)
 }
